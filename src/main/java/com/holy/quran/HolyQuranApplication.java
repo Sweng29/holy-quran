47ecc0a2a0e21c7636e869f984f6a670e31f9e89
Kashif Ali <kashif.ali@venturedive.com>
2021-06-20 14:17:48 +0500

    Initial commit

---
+++ src/main/java/com/holy/quran/HolyQuranApplication.java
@@ -0,0 +1,13 @@
+package com.holy.quran;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HolyQuranApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HolyQuranApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/holy/quran/HolyQuranApplicationTests.java
@@ -0,0 +1,13 @@
+package com.holy.quran;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HolyQuranApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
